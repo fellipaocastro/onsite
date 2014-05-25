7c4b7c0917e708a34dc0bd04421dacd207ff6c3b
Fellipe Castro <contact@fellipecastro.com>
2014-05-25 12:45:05 -0300

    Update repository

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
+HEROKU_URL = 'git@heroku.com:fc-chaordic-onsite.git'
+
 git remote | grep 'heroku' &> /dev/null
+
 if [ $? -ne 0 ]; then
-    git remote add heroku git@heroku.com:fc-chaordic-onsite.git
+    git remote add heroku $HEROKU_URL
 fi
+
 git push heroku master
