dc990131606858df9e2f5f6923f4b20625f18eb7
Fellipe Castro <contact@fellipecastro.com>
2015-06-08 08:15:46 -0300

    Deploys to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
-HEROKU_URL='git@heroku.com:fc-onsite.git'
-
-git remote | grep 'heroku' &> /dev/null
-
-if [ $? -ne 0 ]; then
-    git remote add heroku $HEROKU_URL
-fi
-
-git push heroku master
+git checkout gh-pages
+git merge master
+git push origin gh-pages -u
+git checkout master
